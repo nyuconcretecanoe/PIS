5baf432d5321a637090e75a154c4027b1bf28713
Shubham Parab <71990977+Skparab1@users.noreply.github.com>
2025-01-11 20:57:53 -0800

    graph done

--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,14 @@
 
+
+let tickYarr = [];
+let tickXarr = [];
+let pointsarr = [];
+
+let tickYincrement = 10;
+let tickXincrement = 10;
+
+
+
 function openprefs(){
     document.getElementById('preferences').style.display = 'block';
     document.getElementById('preferences').style.opacity = 1;
@@ -102,6 +112,227 @@ function switchscreens(){
     document.getElementById("screen2").style.display = "block";
 }
 
+function initDraw(){
+    let i = 20;
+    let n = 0;
+    while (i <= 100){
+      drawTickY(0,i,n);
+      n += 1;
+      i += 10*2;
+    }
+    i = 10;
+    n = 0;
+    while (i <= 100){
+      drawTickX(i, n, 0);
+      i += 10;
+    }
+  }
+  
+
+function goodNumber(num){
+    if (num < 100){
+        return "&nbsp&nbsp"+String(num);
+    }
+
+    return String(num);
+}
+
+
+async function growPoint(id){
+
+    let obj = document.getElementById(id);
+
+    let w = 1;
+    while (w < 100){
+    obj.style.width = (w/100)*2.8+"%";
+    await sleep();
+    w += (110-w)/20;
+    }
+} 
+  
+function drawPoint(x, y, n, grow) {
+    const num = n;
+
+    [coordX, coordY] = getcoord(x,y);
+
+    plotgraph.innerHTML += `<span title="(${pointsarr[n][0].toFixed(2)},${pointsarr[n][1].toFixed(2)})"><div onclick="allowClick = false; deletePoint(${num});" onmouseover="glowPoint(${num});" onmouseout="revertPoint(${num});" id="realPoint${num}" class=point style="left: ${coordX}px; top: ${coordY}px;"></div></span>`;
+
+    if (grow){
+        growPoint("realPoint"+num);
+    }
+}
+
+function getcoord(percentX, percentY) {
+
+    // let x = plotgraph.offsetLeft+percentX/100*plotgraph.offsetWidth+1.5;
+    // let y = plotgraph.offsetTop+(1-(percentY/100))*plotgraph.offsetHeight+1.5;
+
+    let x = percentX/100*plotgraph.offsetWidth;
+    let y = (1-(percentY/100))*plotgraph.offsetHeight;
+
+    return [x,y];
+}
+
+  
+function drawTickX(x, y, i, push=true) {
+    if (push){
+        tickXarr.push([x, y]);
+    }
+
+    [coordX, coordY] = getcoord(x,y);
+
+    plotgraph.innerHTML += `
+    <div id="labelX${i}" class=tickLabel style="left: ${coordX-7.75-3}px; top: ${coordY}px;">${Math.round(x*(tickXincrement/10))}</div>
+    <div id="tickX${i}" class=tickX style="left: ${coordX-3}px; top: ${coordY-0.017*plotgraph.offsetHeight}px;"></div>`;
+}
+
+
+function drawTickY(x, y, num, push=true) {
+    if (push){
+        tickYarr.push([x, y]);
+    }
+  
+    [coordX, coordY] = getcoord(x,y);
+
+    // IMPORTANT: HALFS DISPLAY VALUE
+  
+    plotgraph.innerHTML += `
+      <div id="labelY${num}" class=tickLabel style="left: ${coordX-0.03*window.innerWidth}px; top: ${coordY-9.75+2}px;">${goodNumber(Math.round(y*(tickYincrement/10)/2))}</div>
+      <div id="tickY${num}" class=tickY style="left: ${coordX}px; top: ${coordY+2}px;"></div>`;
+}
+
+function drawResid(x, y, num){
+
+    console.log("in resid");
+
+    let [coordX, coordY] = getcoord(x,y);
+
+    let resid = pointsarr[num][1] - ySolution(pointsarr[num][0]);
+
+    const w = pointsarr[num][0]/tickXincrement*10+0.7+"%";
+    let storeh = resid/tickYincrement*10+0.05;
+    //const h = (resid/tickYincrement*10+0.05)+"%";
+
+    // alert(storeh, resid);
+
+    let offset;
+
+    if (resid >= 0){
+        offset = ySolution(pointsarr[num][0])/tickYincrement*10+"%";
+    } else {
+        storeh = -storeh;
+
+        offset = pointsarr[num][1]/tickYincrement*10+"%";
+    }
+
+    storeh = storeh + "%";
+
+
+    const h = storeh;
+
+    const offsetuse = offset;
+
+    console.log("middle of resid");
+
+    residualArr.push(resid);
+
+
+    plotgraph.innerHTML += `
+    <div id="residualLine${num}" class=residualLine style="width: ${w}; height: ${h}; bottom: ${offsetuse}"></div>`;
+
+}
+
+function addpointmanual(x, y){
+
+    let newpts = [x,y];
+    pointsarr.push(newpts);
+
+    redrawlite();
+
+    monitorChange(x,y);
+  }
+
+
+
+function monitorChange(x,y){
+  
+    while (x > tickXincrement*10){
+      tickXincrement = tickXincrement + 1;
+    }
+  
+  
+    while (y > tickYincrement*5){
+      tickYincrement = tickYincrement + 1;
+    }
+  
+    redraw();
+}
+
+
+
+function redraw(pointgrow = null){
+
+    plotgraph.innerHTML = ``;
+  
+    let i = 0;
+    for (tick of tickYarr){
+      drawTickY(tick[0], tick[1], i, false);
+      i += 1;
+    }
+  
+    i = 0;
+    for (tick of tickXarr){
+      drawTickX(tick[0], tick[1], i, false);
+      i += 1;
+    }
+  
+    console.log(pointgrow)
+  
+    i = 0;
+    for (point of pointsarr){
+      drawPoint(point[0]/tickXincrement*10, point[1]/tickYincrement*10*2, i, (pointgrow == i));
+      i += 1;
+    }
+  
+    plotgraph.innerHTML += `<div id="tickedBox" class="tickedBox"></div>`;
+  
+}
+
+// lite redraw
+function redrawlite(){
+  
+    let i = pointsarr.length-1;
+  
+    console.log("point "+i+" being drawn");
+    point = pointsarr[i];
+    drawPoint(point[0]/tickXincrement*10, point[1]/tickYincrement*10*2, i, true)
+}
+
+function makedict(datastr){
+    let spltup = datastr.split('\n');
+
+    let enddict = {
+        "time": [],
+        "seconds_elapsed": [],
+        "x": [],
+        "y": [],
+        "z": []
+    };
+
+    let i = 1;
+    while (i < spltup.length){
+        let line = spltup[i].split(',');
+        enddict.time.push(line[0]);
+        enddict.seconds_elapsed.push(line[1]);
+        enddict.x.push(line[2]);
+        enddict.y.push(line[3]);
+        enddict.z.push(line[4]);
+        i += 1;
+    }
+
+    return enddict;
+}
+
 
 let phoneAccelerometerCSV;
 let phoneGyroscopeCSV;
@@ -158,6 +389,8 @@ while (i < 7){
     });
     i += 1;
 }
+
+initDraw();
 
 
 
